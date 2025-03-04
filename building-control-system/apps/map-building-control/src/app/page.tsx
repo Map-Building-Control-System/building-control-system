fee4f4590e98345bcc49fdedafaea5fab65fe84b
Abdullah Mutlu <abdullah.mutllu@gmail.com>
2025-03-04 23:06:22 +0300

    .

---
+++ building-control-system/apps/map-building-control/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import { Auth } from '@building-control-system/auth';
+
+export default function Index() {
+  return (
+    <div>
+      <Auth />  {/* Kütüphanedeki bileşeni kullanın */}
+    </div>
+  );
+}
---
+++ building-control-system/libs/auth/src/lib/auth.tsx
@@ -0,0 +1,10 @@
+import styles from './auth.module.css';
+export function Auth() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Authh!</h1>
+    </div>
+  );
+}
+
+export default Auth;
